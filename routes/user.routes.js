a598edd16edb7e61e5d8052461de467dd5e5979c
marco <marcoreda56@gmail.com>
2026-06-22 14:11:30 +0300

    add all routes of user

---
+++ routes/user.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+const { protect } = require('../middlewares/auth.middleware');
+const { isAdmin } = require('../middlewares/admin.middleware');
+const upload = require('../middlewares/upload.middleware');
+
+router.use(protect);
+
+router.get('/me', userController.getMe);
+router.patch('/me', userController.updateMe);
+router.post('/me/profile-image', upload.single('image'), userController.uploadProfileImage);
+router.post('/me/change-password', userController.changePassword);
+
+// Admin only
+router.get('/', isAdmin, userController.getAllUsers);
+router.patch('/:userId/status', isAdmin, userController.updateUserStatus);
+
+module.exports = router;
